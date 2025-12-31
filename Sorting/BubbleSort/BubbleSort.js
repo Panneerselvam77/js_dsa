f8729c433e70ce6a6dac4442b32cb02ca14c4638
Panneerselvam <panneerdeveloper77@gmail.com>
2025-12-31 08:21:18 +0530

    bubble sort

---
+++ Sorting/BubbleSort/BubbleSort.js
@@ -0,0 +1,36 @@
+function bubbleSort(arr) {
+  if (!arr) return [];
+
+  const n = arr.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+function bubbleSort(arr) {
+  if (!arr) return [];
+
+  const n = arr.length;
+
+  for (let i = 0; i < n; i++) {
+    let swapped = false;
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        swapped = true;
+      }
+    }
+    if (!swapped) break;
+  }
+  return arr;
+}
+let arr = [7, 3, 9, 12, 11];
+console.log(bubbleSort(arr));
