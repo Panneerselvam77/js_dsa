32f630d16ab9fe926e009641bd678fd83e97b797
Panneerselvam <panneerdeveloper77@gmail.com>
2026-05-02 17:30:17 +0530

    min, max, delete - add

--- Tree/BST.js
+++ Tree/BST.js
@@ -85,6 +85,39 @@ class Tree {
     }
     return result;
   }
+  min(root) {
+    if (!root.left) return root.value;
+    else return this.min(root.left);
+  }
+  max(root) {
+    if (!root.right) return root.value;
+    else return this.max(root.right);
+  }
+  delete(value) {
+    this.root = this.deleteNode(this.root, value);
+  }
+  deleteNode(root, value) {
+    if (root === null) {
+      return root;
+    }
+    if (value < root.value) {
+      root.left = this.deleteNode(root.left, value);
+    } else if (value > root.value) {
+      root.right = this.deleteNode(root.right, value);
+    } else {
+      if (!root.left && !root.right) {
+        return null;
+      }
+      if (!root.left) {
+        return root.right;
+      } else if (!root.right) {
+        return root.left;
+      }
+      root.value = this.min(root.right);
+      root.right = this.deleteNode(root.right, root.value);
+    }
+    return root;
+  }
 }
 
 const BSt = new Tree();
