24fb54e8f6683ad9a079fe8814a96989d3b2de36
Panneerselvam <panneerdeveloper77@gmail.com>
2026-05-02 09:37:13 +0530

    BST implementation

---
+++ Tree/BST.js
@@ -0,0 +1,55 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+  isEmpty() {
+    return this.root === null;
+  }
+  insert(value) {
+    const newNode = new Node(this.value);
+    if (this.isEmpty()) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+  insertNode(root, newNode) {
+    if (newNode.value < root.value) {
+      if (root.left === null) {
+        root.left = newNode;
+      } else {
+        this.insertNode(root.left, newNode);
+      }
+    } else if (newNode.value > root.value) {
+      if (root.right === null) {
+        root.right = newNode;
+      } else {
+        this.insertNode(root.right, newNode);
+      }
+    }
+    return this;
+  }
+  search(root, value) {
+    if (!root) return false;
+    if (root.value === value) return true;
+    if (value < root.value) {
+      return this.search(root.left, value);
+    } else {
+      return this.search(root.right, value);
+    }
+  }
+}
+
+const BSt = new Tree();
+BSt.insert(10);
+BSt.insert(5);
+BSt.insert(15);
+console.log(BSt);
